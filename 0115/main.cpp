d06a36b4600feaf8dce2094d7c575e4af61d0ff0
Hiroyuki Sano <sh19910711@gmail.com>
2014-02-07 00:01:10 +0900

    AOJ 0115: unsubmitted

---
+++ 0115/main.cpp
@@ -0,0 +1,269 @@
+
+// @snippet<sh19910711/contest-base:headers.cpp>
+#define __THIS_IS_CPP11__
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+#ifdef __THIS_IS_CPP11__
+#include <memory>
+#include <array>
+#endif
+
+
+// @snippet<sh19910711/contest:utils/collection_prototype.cpp>
+namespace utils {
+  // collection
+#ifdef __THIS_IS_CPP11__
+  // fill(begin, num, value)
+  template <class T, class V> void fill( T& v, const int& n, const V& value );
+  // copy(from, to), copy(from, num, to)
+  template <class T> void copy( const T& s, T& t );
+  template <class T> void copy( const T& s, const int& n, T& t );
+  // sort(A), sort(begin, num)
+  template <class T> void sort( T& v );
+  template <class T> void sort( T& v, const int& n );
+#endif
+}
+
+// @snippet<sh19910711/contest:utils/misc_prototype.cpp>
+namespace utils {
+  // max(a, b), min(a, b)
+  template <class T> void max( T& a, const T& b );
+  template <class T> void min( T& a, const T& b );
+  // to_s(v), ret = to_type(str)
+  template <class T> std::string to_string( const T& v );
+  template <class T> T to_type( const std::string& s );
+  
+}
+
+// @snippet<sh19910711/contest:utils_prototypes.cpp>
+
+// @snippet<sh19910711/contest-base:solution/template-class.cpp>
+namespace solution {
+  struct Input;
+  struct Output;
+  struct Storages {
+    Input* in;
+    Output* out;
+  };
+}
+
+// @snippet<sh19910711/contest-base:solution/solver-interface.cpp>
+namespace solution {
+  struct SolverInterface {
+    virtual ~SolverInterface() {}
+    virtual const Output* solve( const Input* in, Output* out ) = 0;
+  };
+}
+
+// @snippet<sh19910711/contest-base:solution/solution-interface.cpp>
+namespace solution {
+  struct SolutionInterface {
+    virtual ~SolutionInterface() {};
+    virtual int run()                             = 0;
+    virtual bool action()                         = 0;
+    virtual bool input( Input* s )         = 0;
+    virtual void output( const Output* s ) = 0;
+    virtual void before_all_actions() {}
+    virtual void after_all_actions() {}
+    virtual void before_action( const int& test_no ) {}
+    virtual void after_action( const int& test_no ) {}
+    SolverInterface* solver;
+    Storages* storages;
+  };
+}
+
+// @snippet<sh19910711/contest-base:solution/solution-base.cpp>
+namespace solution {
+  struct SolutionBase: SolutionInterface {
+    virtual ~SolutionBase() {};
+    
+    int run() {
+      this->before_all_actions();
+      for (;;) {
+        this->before_action(-1);
+        if ( ! this->action() ) { this->after_action(-1); break; }
+        this->after_action(-1);
+      };
+      this->after_all_actions();
+      return 0;
+    }
+    
+    bool action() {
+      if ( ! this->input(this->storages->in) ) {
+        return false;
+      }
+      this->output(solver->solve(this->storages->in, this->storages->out));
+      return true;
+    }
+  };
+}
+
+// @snippet<sh19910711/contest-base:solution/template-primitive-types.cpp>
+namespace solution {
+  typedef long long Int;
+  typedef long double Double;
+  typedef std::string String;
+}
+
+// @snippet<sh19910711/contest-base:solution/template-constants.cpp>
+namespace solution {
+  // constants
+  const Int MAX_D = 3;
+  const Int MAX_V = 3;
+  const String YES = "HIT";
+  const String NO = "MISS";
+}
+
+// @snippet<sh19910711/contest-base:solution/template-namespaces.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace utils;
+  typedef std::array<Double, MAX_D> DoubleArray;
+  typedef std::array<DoubleArray, MAX_V> DoubleTable;
+}
+
+// @snippet<sh19910711/contest-base:solution/template-storage.cpp>
+namespace solution {
+  struct Input {
+    DoubleArray S;
+    DoubleArray T;
+    DoubleTable P;
+  };
+  
+  struct Output {
+    bool result;
+  };
+  
+}
+
+namespace geometry {
+  typedef long double Double;
+
+  typedef std::tuple<Double, Double> Vector2DBase;
+  struct Vector2D: Vector2DBase {
+    using Vector2DBase::tuple;
+    Double& get_x() { return std::get<0>(*this); }
+    const Double& get_x() const { return std::get<0>(*this); }
+    Double& get_y() { return std::get<1>(*this); }
+    const Double& get_y() const { return std::get<1>(*this); }
+  };
+
+  typedef std::tuple<Double, Double, Double> Vector3DBase;
+  struct Vector3D: Vector3DBase {
+    using Vector3DBase::tuple;
+    Double& get_x() { return std::get<0>(*this); }
+    const Double& get_x() const { return std::get<0>(*this); }
+    Double& get_y() { return std::get<1>(*this); }
+    const Double& get_y() const { return std::get<1>(*this); }
+    Double& get_z() { return std::get<2>(*this); }
+    const Double& get_z() const { return std::get<2>(*this); }
+  };
+
+  template <class Point> Point get_middle( Point a, Point b ) {
+    return ( a + b ) / 2.0;
+  }
+}
+
+// @snippet<sh19910711/contest-base:solution/template-solver-area.cpp>
+namespace solution {
+  struct Solver: SolverInterface {
+    const Output* solve( const Input* in, Output* out ) {
+      out->result = is_intersect(in->S, in->T, in->P);
+      return out;
+    }
+
+    static bool is_intersect( const DoubleArray& S, const DoubleArray& T, const DoubleTable& P ) {
+      return false;
+    }
+  };
+}
+
+// @snippet<sh19910711/contest-base:solution/template-solution.cpp>
+namespace solution {
+  struct Solution: SolutionBase {
+    bool input( Input* in ) {
+      if ( ! ( std::cin >> in->S[0] >> in->S[1] >> in->S[2] ) )
+        return false;
+      if ( ! ( std::cin >> in->T[0] >> in->T[1] >> in->T[2] ) )
+        return false;
+      for ( auto i = 0; i < MAX_V; ++ i ) {
+        for ( auto j = 0; j < MAX_D; ++ j ) {
+          std::cin >> in->P[i][j];
+        }
+      }
+      return true;
+    }
+    void output( const Output* out ) {
+      std::cout << ( out->result ? YES : NO ) << std::endl;
+    }
+  };
+}
+
+// @snippet<sh19910711/contest-base:main_func.cpp>
+#ifndef __MY_UNIT_TEST__
+int main() {
+  // desc: http://meme.biology.tohoku.ac.jp/students/iwasaki/cxx/speed.html#id18
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(0);
+  // 
+  using namespace solution;
+  static Input in;
+  static Output out;
+  static Storages storages;
+  static Solution solution;
+  static Solver solver;
+  storages.in = &in;
+  storages.out = &out;
+  solution.storages = &storages;
+  solution.solver = &solver;
+  return solution.run();
+}
+#endif
+
+// @snippet<sh19910711/contest:utils/collection.cpp>
+namespace utils {
+#ifdef __THIS_IS_CPP11__
+  // fill(begin, num, value)
+  template <class T, class V> void fill( T& v, const int& n, const V& value ) { std::fill(std::begin(v), std::begin(v) + n, value); }
+  // copy(from, to), copy(from, num, to)
+  template <class T> void copy( const T& s, T& t ) { std::copy(std::begin(s), std::end(s), std::begin(t)); }
+  template <class T> void copy( const T& s, const int& n, T& t ) { std::copy(std::begin(s), std::begin(s) + n, std::begin(t)); }
+  // sort(A), sort(begin, num)
+  template <class T> void sort( T& v ) { std::sort(std::begin(v), std::end(v)); }
+  template <class T> void sort( T& v, const int& n ) { std::sort(std::begin(v), std::begin(v) + n); }
+#endif
+}
+
+// @snippet<sh19910711/contest:utils/misc.cpp>
+namespace utils {
+  // max(a, b), min(a, b)
+  template <class T> void max( T& a, const T& b ) { a = std::max(a, b); }
+  template <class T> void min( T& a, const T& b ) { a = std::min(a, b); }
+  // to_s(v), ret = to_type(str)
+  template <class T> std::string to_string( const T& v ) { std::ostringstream oss; oss << v; return oss.str(); }
+  template <class T> T to_type( const std::string& s ) { std::istringstream iss(s); T res; iss >> res; return res; }
+  
+}
+
+// @snippet<sh19910711/contest:utils.cpp>
+
+// @snippet<sh19910711/contest-base:main.cpp>
+
